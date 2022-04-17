2c86afc7e260b87178ea349c4c55e5fada2c510d
caaatisgood <almigod0718@gmail.com>
2022-04-17 23:28:25 +0100

    It looks like some goddamn flying hotdogs

--- likewater22/src/sketch.js
+++ likewater22/src/sketch.js
@@ -7,6 +7,7 @@ let cheight = cwidth
 let streams = []
 let pg = 0
 let bg = "#edf6f9"
+bg = "#000"
 	
 /*
 ------------------------------------------------------------
@@ -28,7 +29,7 @@ export function setup() {
 	background(bg);
   drawingContext.shadowBlur = 5;
   clrs = random(clrStrs).split("/").pop().split("-").map(s => "#"+s)
-  let streamsAmt = 20
+  let streamsAmt = 30
   for (let i = 0; i<streamsAmt; i++) {
     streams.push(
       new Stream({
@@ -44,7 +45,7 @@ export function setup() {
   }
 }
 
-const STREAM_LENGTH = 30 // particle amount
+const STREAM_LENGTH = 15 // particle amount
 class Stream {
   constructor({ idx, particleConfig } = {}) {
     let particles = []
@@ -74,7 +75,7 @@ class Stream {
 }
 
 let PARTICLE_RANGE_LENGTH = 0.3 // 0~n
-let PARTICLE_PG_OFFSET_SCALE = 0.2
+let PARTICLE_PG_OFFSET_SCALE = 0.3
 class Particle {
 	constructor({ streamIdx, idx, pgOffset, initX = width/2, initY = height/2 } = {}) {
     // pgRange: pgOffset + [fixed range]
@@ -84,7 +85,7 @@ class Particle {
       pgRangeOffset,
       pgRangeOffset + PARTICLE_RANGE_LENGTH
     ]
-    let offsetBase = 0 + streamIdx /10
+    let offsetBase = 0 + streamIdx/10*0
 		let def = {
       streamIdx,
       idx,
@@ -93,9 +94,9 @@ class Particle {
       initY,
       yOffsetRange: [offsetBase, offsetBase],
       xOffsetRange: [offsetBase, offsetBase],
-      piRange: [0, 2*PI],
-      xScaleRange: [300, 300],
-      yScaleRange: [300, 300],
+      piRange: [0, 4*PI],
+      xScaleRange: [200, 300],
+      yScaleRange: [200, 300],
       clr: random(clrs),
 		}
 		Object.assign(this, def)
@@ -121,19 +122,22 @@ class Particle {
     let [yScaleStart, yScaleEnd] = yScaleRange
     push()
       let noiseScale = 130
-      noiseScale = 900
+      noiseScale = map(pg, 0, 1, 200, 800)
       let xDeg = map(selfPg, 0, 1, piStart, piEnd, true)
       let xScale = map(selfPg, 0, 1, xScaleStart, xScaleEnd, true)
       let _x =
         cos(xDeg) * xScale +
-        (noise((1-selfPg)*5/7, streamIdx * 0.1) - 0.5) * noiseScale
+        (noise((1-selfPg)*5/5, streamIdx * 0.1) - 0.5) * noiseScale
       let yDeg = map(selfPg, 0, 1, piStart, piEnd, true)
       let yScale = map(selfPg, 0, 1, yScaleStart, yScaleEnd, true)
       let _y =
         sin(yDeg) * yScale +
-        (noise((1-selfPg)*10/7, streamIdx * 0.1) - 0.5) * noiseScale
+        (noise((1-selfPg)*10/5, streamIdx * 0.1) - 0.5) * noiseScale
       let xOffset = lerp(...xOffsetRange, selfPg)
       let yOffset = lerp(...yOffsetRange, selfPg)
+      if (streamIdx === 0) {
+        // console.log(yOffset)
+      }
       let x = initX + xOffset + _x
       let y = initY + yOffset + _y
       // strokeWeight(0.3)
@@ -145,13 +149,13 @@ class Particle {
         let clr1 = color("#f94144")
         let clr2 = color("#011627")
         let _clr = lerpColor(clr1, clr2, selfPg)
-        _clr.setAlpha(200)
+        _clr.setAlpha(150)
         fill(_clr)
         rotate(xDeg + PI/2 * 0.9 + noise(yDeg/3+streamIdx/10)/3)
         let w = 30 + noise(selfPg + streamIdx * 2) * 50
         let h = lerp(4, 2, selfPg) + noise(xDeg, idx*3)*8
         rectMode(CENTER)
-        rect(0, 0, w, h)
+        rect(0, 0, w, h, 5)
         // ellipse(0, 0, h*2)
       pop()
     pop()
@@ -166,7 +170,7 @@ export function draw() {
     return
   } else {
     // pg = map(mouseX, 0, width, 0, 1)
-    pg = map(cos(frameCount/50), -1, 1, 0, 1)
+    pg = map(cos(frameCount/100), -1, 1, 0, 1)
   }
 	streams.forEach(p => {
     p.draw()
