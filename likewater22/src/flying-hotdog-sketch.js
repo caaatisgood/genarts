05a88ea263fd5ab8d4047755c406ed67242738dd
caaatisgood <almigod0718@gmail.com>
2022-04-18 09:02:30 +0100

    Okay, literally still duno wats goin on

--- likewater22/src/flying-hotdog-sketch.js
+++ likewater22/src/flying-hotdog-sketch.js
@@ -7,6 +7,21 @@ let cheight = cwidth
 let streams = []
 let pg = 0
 let bg = "#edf6f9"
+bg = "#000"
+	
+/*
+------------------------------------------------------------
+0                  [-------------]
+1                [-------------]
+2              [-------------]
+3            [-------------]
+4          [-------------]
+5        [-------------]
+6      [-------------]
+7    [-------------]
+8  [-------------]
+9...
+*/
 
 export function setup() {
   cheight = cwidth = min(windowWidth, windowHeight)
@@ -14,21 +29,23 @@ export function setup() {
 	background(bg);
   drawingContext.shadowBlur = 5;
   clrs = random(clrStrs).split("/").pop().split("-").map(s => "#"+s)
-  let streamsAmt = 20
+  let streamsAmt = 30
   for (let i = 0; i<streamsAmt; i++) {
     streams.push(
       new Stream({
         idx: i,
+        // x: width/2 + noise(i*10, i) * map(noise(i/10, i), 0, 1, 0, 10),
+        // y: height/2 + noise(i*20, i) * map(noise(i/10, i), 0, 1, 0, 10),
         particleConfig: {
-          initX: width/2 + sin(i/3)*30-30,
-          initY: height/2 + cos(i/2)*30-50,
+          initX: width/2 + sin(i/3)*10-30,
+          initY: height/2 + cos(i/2)*10-50,
         },
       })
     )
   }
 }
 
-const STREAM_LENGTH = 50 // particle amount
+const STREAM_LENGTH = 15 // particle amount
 class Stream {
   constructor({ idx, particleConfig } = {}) {
     let particles = []
@@ -77,9 +94,9 @@ class Particle {
       initY,
       yOffsetRange: [offsetBase, offsetBase],
       xOffsetRange: [offsetBase, offsetBase],
-      piRange: [0, 7*PI],
-      xScaleRange: [350, 250],
-      yScaleRange: [350, 250],
+      piRange: [0, 4*PI],
+      xScaleRange: [250, 150],
+      yScaleRange: [250, 150],
       clr: random(clrs),
 		}
 		Object.assign(this, def)
@@ -105,20 +122,26 @@ class Particle {
     let [yScaleStart, yScaleEnd] = yScaleRange
     push()
       let noiseScale = 130
-      noiseScale = map(pg, 0, 1, 100, 1000)
+      noiseScale = map(pg, 0, 1, 400, 700)
       let deg = map(selfPg, 0, 1, piStart, piEnd, true) + streamIdx/10
       let xScale = map(selfPg, 0, 1, xScaleStart, xScaleEnd, true)
       let _x =
-        cos(deg) * xScale * (1-pg) +
+        cos(deg) * xScale +
         (noise((1-selfPg)*5/4, streamIdx * 0.1) - 0.5) * noiseScale
       let yScale = map(selfPg, 0, 1, yScaleStart, yScaleEnd, true)
       let _y =
-        sin(deg) * yScale * pg +
+        sin(deg) * yScale +
         (noise((1-selfPg)*10/4, streamIdx * 0.1) - 0.5) * noiseScale
       let xOffset = lerp(...xOffsetRange, selfPg)
       let yOffset = lerp(...yOffsetRange, selfPg)
+      if (streamIdx === 0) {
+        // console.log(yOffset)
+      }
       let x = initX + xOffset + _x
       let y = initY + yOffset + _y
+      // strokeWeight(0.3)
+      let strkClr = color(clr)
+      // stroke(strkClr)
       noStroke()
       push()
         translate(x, y)
@@ -128,8 +151,8 @@ class Particle {
         _clr.setAlpha(150)
         fill(_clr)
         rotate(deg + PI/2 * 0.9 + noise(deg/3+streamIdx/10)/3)
-        let w = 10 + noise(selfPg + streamIdx * 30) * 10
-        let h = lerp(4, 2, selfPg) + noise(deg, idx*3)*2
+        let w = 30 + noise(selfPg + streamIdx * 30) * 20
+        let h = lerp(4, 2, selfPg) + noise(deg, idx*3)*8
         rectMode(CENTER)
         rect(0, 0, w, h, 5)
         // ellipse(0, 0, h*2)
