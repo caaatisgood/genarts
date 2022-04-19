7de844792bb6a759f1d7ba8cdc7f655d75822269
caaatisgood <almigod0718@gmail.com>
2022-04-19 23:19:02 +0100

    Modualize

--- likewater22/src/index.js
+++ likewater22/src/index.js
@@ -1,5 +1,41 @@
-import { setup, draw, mousePressed } from './sketch'
+import sketch from './sketch'
+new p5(sketch)
 
-window.setup = setup
-window.draw = draw
-window.mousePressed = mousePressed
+window.DEVICE_MOTION_TOUCHED = false
+window.DEVICE_MOTION_SUPPORTED = false
+
+function deviceMotionHandler(evt) {
+  if (!evt?.accelerationIncludingGravity) {
+    return
+  }
+  let acc = evt.accelerationIncludingGravity
+  window.DEVICE_MOTION_TOUCHED = true
+}
+
+function subscribeDeviceMotion() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        alert(`> permissionState: ${permissionState}`);
+        if (permissionState === "granted") {
+          window.addEventListener("devicemotion", deviceMotionHandler, true);
+        }
+      })
+      .catch(err => {
+        alert(err)
+      });
+  } else {
+    alert(`DeviceMotion is not supported on this device 😢`);
+  }
+}
+
+if (typeof DeviceMotionEvent.requestPermission === "function") {
+  // window.DEVICE_MOTION_SUPPORTED = true
+  // document.getElementById("dm-block").style.display = 'flex'
+  // document.querySelector("#dm-block button").onclick = subscribeDeviceMotion
+}
+
+// 8 + 8 + 8
+// morning: 08 ~ 16
+// evening: 16 ~ 00
+//   night: 00 ~ 08
--- likewater22/src/sketch.js
+++ likewater22/src/sketch.js
@@ -13,151 +13,154 @@ let DRAWING_SCALE
 let MOUSE_TOUCHED = false
 let CLR_ALPHA
 
-export function setup() {
-  randomSeed(4222022)
-  noiseSeed(4222022)
-  pixelDensity(4)
+function setup(p5) {
+
+  // define particle
+  class Particle {
+    constructor({ idx, initX, initY, clr } = {}) {
+      let currv = p5.createVector(initX, initY)
+      let accv = p5.createVector(1, 0)
+      let def = {
+        idx,
+        currv,
+        accv,
+        w: 0,
+        h: 0,
+        rot: 0,
+        clr,
+        flashy: p5.random() < 0.66,
+        on: true,
+        overallNoise: 12,
+      }
+      Object.assign(this, def)
+    }
+    
+    update() {
+      if (this._isDone()) {
+        return
+      }
+
+      const { idx, currv, accv, flashy, on } = this
+      if (flashy && (p5.frameCount % 3) === 0) {
+        if (on && p5.random() > 0.6) {
+          this.on = false
+        } else if (!on && p5.random() > 0.3) {
+          this.on = true
+        }
+      }
+
+      if (currv.x > p5.width) {
+        currv.set([0, currv.y])
+      } else if (currv.x < 0) {
+        currv.set([p5.width, currv.y])
+      } else if (currv.y > p5.height) {
+        currv.set([currv.x, 0])
+      } else if (currv.y < 0) {
+        currv.set([currv.x, p5.height])
+      } else {
+        let xBaseSpeed =
+          p5.frameCount > 20 && MOUSE_TOUCHED
+            ? p5.map(p5.mouseX, 0, p5.width, -0.5, 0.6)
+            : 0.2
+        let accX =
+          xBaseSpeed + p5.sin((p5.frameCount+idx)/40)/2 +
+          p5.noise(currv.x, p5.sin((p5.frameCount+idx*10)/50)) * 2
+        let overallNoise = this.overallNoise
+
+        let yWaviness =
+          MOUSE_TOUCHED
+            ? p5.map((p5.mouseY - p5.height / 2)/p5.height, -0.5, 0.5, -40, 40)
+            : p5.noise(p5.frameCount/80) * 10
+        let accY =
+          p5.sin((p5.frameCount+idx)/1)*0.5 +
+          (p5.noise(currv.y/overallNoise+currv.x/(50+idx*200), p5.cos((p5.frameCount+idx*100)/50)*10) - 0.5) *
+          p5.noise(p5.frameCount/300)*yWaviness
+        let accXScale = 1.2
+        accv.set([
+          accX * accXScale * DRAWING_SCALE,
+          accY * DRAWING_SCALE
+        ])
+        currv.add(accv)
+        this.w = (0.3 + p5.noise(p5.frameCount/30, idx/100) * 3) * DRAWING_SCALE
+        this.h = p5.noise((p5.frameCount+idx*10)/30) * 12 * DRAWING_SCALE
+        this.rot = p5.sin((idx+currv.y)/30)*p5.PI*2 + p5.noise((idx+currv.y)/20)*p5.PI*3
+      }
+    }
+
+    draw() {
+      if (this._isDone()) {
+        return
+      }
+    
+      const { currv, clr, w, h, rot, on } = this
+      if (!on) {
+        return
+      }
+      p5.push()
+        let [x, y] = this._applyPadding(currv.x, currv.y)
+        p5.translate(x, y)
+        p5.noStroke()
+        clr.setAlpha(CLR_ALPHA)
+        p5.fill(clr)
+        p5.rectMode(p5.CENTER)
+        p5.rotate(rot)
+        p5.rect(0, 0, w, h)
+        p5.pop()
+    }
+
+    _isDone() {
+      return this.currv.x > p5.width
+    }
+
+    _applyPadding(x, y) {
+      let wPadding = 0.07
+      let hPadding = 0.05
+      return [
+        p5.width * wPadding + x * (1-wPadding*2),
+        p5.height * hPadding + y * (1-hPadding*2),
+      ]
+    }
+  }
+
+  // main setup
+  p5.randomSeed(4222022)
+  p5.noiseSeed(4222022)
+  p5.pixelDensity(4)
   let ASPECT_RATIO = 0.77 // 0.77 : 1 (w:h)
-  if (windowWidth / windowHeight > ASPECT_RATIO) {
-    cheight = windowHeight
+  if (p5.windowWidth / p5.windowHeight > ASPECT_RATIO) {
+    cheight = p5.windowHeight
     cwidth = cheight * ASPECT_RATIO
   } else {
-    cwidth = windowWidth
-    cheight = windowHeight / ASPECT_RATIO
+    cwidth = p5.windowWidth
+    cheight = p5.windowHeight / ASPECT_RATIO
   }
   DRAWING_SCALE = cheight / 1000
-  createCanvas(cwidth, cheight);
+  p5.createCanvas(cwidth, cheight);
   let particlesAmt = 150
-  let [clr1, clr2, bg, clrAlpha] = random(clrs)
+  let [clr1, clr2, bg, clrAlpha] = p5.random(clrs)
   CLR_ALPHA = clrAlpha
-	background(bg);
-  let _clr1 = color(clr1)
-  let _clr2 = color(clr2)
+	p5.background(bg);
+  let _clr1 = p5.color(clr1)
+  let _clr2 = p5.color(clr2)
   for (let i = 0; i<particlesAmt; i++) {
-    let ypg = random(0, 1)
-    let clr = lerpColor(_clr1, _clr2, ypg)
+    let ypg = p5.random(0, 1)
+    let clr = p5.lerpColor(_clr1, _clr2, ypg)
     particles.push(
       new Particle({
         idx: i,
         initX: 0,
-        initY: ypg * height,
+        initY: ypg * p5.height,
         clr,
       })
     )
   }
 }
 
-class Particle {
-  constructor({ idx, initX, initY, clr } = {}) {
-    let currv = createVector(initX, initY)
-    let accv = createVector(1, 0)
-    let def = {
-      idx,
-      currv,
-      accv,
-      w: 0,
-      h: 0,
-      rot: 0,
-      clr,
-      flashy: random() < 0.66,
-      on: true,
-      overallNoise: 12,
-    }
-    Object.assign(this, def)
-  }
-  
-  update() {
-    if (this._isDone()) {
-      return
-    }
-
-    const { idx, currv, accv, flashy, on } = this
-    if (flashy && (frameCount % 3) === 0) {
-      if (on && random() > 0.6) {
-        this.on = false
-      } else if (!on && random() > 0.3) {
-        this.on = true
-      }
-    }
-
-    if (currv.x > width) {
-      currv.set([0, currv.y])
-    } else if (currv.x < 0) {
-      currv.set([width, currv.y])
-    } else if (currv.y > height) {
-      currv.set([currv.x, 0])
-    } else if (currv.y < 0) {
-      currv.set([currv.x, height])
-    } else {
-      let xBaseSpeed =
-        frameCount > 20 && MOUSE_TOUCHED
-          ? map(mouseX, 0, width, -0.5, 0.6)
-          : 0.2
-      let accX =
-        xBaseSpeed + sin((frameCount+idx)/40)/2 +
-        noise(currv.x, sin((frameCount+idx*10)/50)) * 2
-      let overallNoise = this.overallNoise
-
-      let yWaviness =
-        MOUSE_TOUCHED
-          ? map((mouseY - height / 2)/height, -0.5, 0.5, -40, 40)
-          : noise(frameCount/80) * 10
-      let accY =
-        sin((frameCount+idx)/1)*0.5 +
-        (noise(currv.y/overallNoise+currv.x/(50+idx*200), cos((frameCount+idx*100)/50)*10) - 0.5) *
-        noise(frameCount/300)*yWaviness
-      let accXScale = 1.2
-      accv.set([
-        accX * accXScale * DRAWING_SCALE,
-        accY * DRAWING_SCALE
-      ])
-      currv.add(accv)
-      this.w = (0.3 + noise(frameCount/30, idx/100) * 3) * DRAWING_SCALE
-      this.h = noise((frameCount+idx*10)/30) * 12 * DRAWING_SCALE
-      this.rot = sin((idx+currv.y)/30)*PI*2 + noise((idx+currv.y)/20)*PI*3
-    }
-  }
-
-  draw() {
-    if (this._isDone()) {
-      return
-    }
-  
-    const { currv, clr, w, h, rot, on } = this
-    if (!on) {
-      return
-    }
-    push()
-      let [x, y] = this._applyPadding(currv.x, currv.y)
-      translate(x, y)
-      noStroke()
-      clr.setAlpha(CLR_ALPHA)
-      fill(clr)
-      rectMode(CENTER)
-      rotate(rot)
-      rect(0, 0, w, h)
-    pop()
-  }
-
-  _isDone() {
-    return this.currv.x > width
-  }
-
-  _applyPadding(x, y) {
-    let wPadding = 0.08
-    let hPadding = 0.05
-    return [
-      width * wPadding + x * (1-wPadding*2),
-      height * hPadding + y * (1-hPadding*2),
-    ]
-  }
-}
-
-export function draw() {
-  if (!MOUSE_TOUCHED && (mouseX !== 0 || mouseY !== 0)) {
+function draw(p5) {
+  if (!MOUSE_TOUCHED && (p5.mouseX !== 0 || p5.mouseY !== 0)) {
     MOUSE_TOUCHED = true
   }
-  if (DEVICE_MOTION_SUPPORTED && !DEVICE_MOTION_TOUCHED) {
+  if (window.DEVICE_MOTION_SUPPORTED && !window.DEVICE_MOTION_TOUCHED) {
     return
   }
   let isDone = true
@@ -167,13 +170,19 @@ export function draw() {
     isDone = isDone && p._isDone()
   })
   if (isDone) {
-    noLoop()
+    p5.noLoop()
   }
 }
 
-export function mousePressed() {
-  const dateTime = (
-    new Date().toDateString() + " " + new Date().toLocaleTimeString().replace(/\:/g, "")
-  ).split(" ").join("-")
-  // save(`defnotlikewater22-${dateTime}`)
+const sketch = (p5) => {
+  p5.setup = () => setup(p5)
+  p5.draw = () => draw(p5)
+  p5.mousePressed = () => {
+    const dateTime = (
+      new Date().toDateString() + " " + new Date().toLocaleTimeString().replace(/\:/g, "")
+    ).split(" ").join("-")
+    // save(`defnotlikewater22-${dateTime}`)
+  }
 }
+
+export default sketch
